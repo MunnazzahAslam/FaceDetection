92bc46a654d4631d0fc4d9afad4c721bbcc7fa02
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-08-24 23:01:35 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const video= document.getElementById("video");
 
-
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
     faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
@@ -10,16 +9,8 @@ Promise.all([
   ]).then(startVideo);
   
   function startVideo() {
-    navigator.mediaDevices.getUserMedia(
-      { video: {} },
-      stream => (video.srcObject = stream),
-      err => console.error(err)
-    );
-
-    }
-//function startStream(){
-  //  navigator.mediaDevices.getUserMedia({video: {}}) .then((stream)=> {video.srcObject = stream;}, (err)=> console.error(err));
-//}
+   navigator.mediaDevices.getUserMedia({video: {}}) .then((stream)=> {video.srcObject = stream;}, (err)=> console.error(err));
+}
 video.addEventListener("playing", () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
